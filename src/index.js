388eb72249a104826ff9d71d66954b737d219759
Youhei Nakagawa <youhei.nakagawa@gmail.com>
2020-07-05 13:19:21 +0900

    modify test case

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const bridgeUrl = 'ftp://bridge.youhei-nakagawa.net/pub/open.html?url=';
 
-function g2s(ua, selector) {
-  if (g2sCheckTarget(ua)) {
+function g2s(selector, ua = window.navigator.userAgent) {
+  if (checkTarget(ua)) {
     var matches = document.querySelectorAll(selector);
     matches.forEach((item) => {
       if (item.href) {
         item.href = bridgeUrl + encodeURIComponent(item.href);
+        return true;
       }
     });
   }
+  return false;
 }
 
 function checkTarget(ua) {
-  console.log(ua);
   const safari = /safari\//.test(ua.toLowerCase());
   const ios = /iphone|ipod|ipad/.test(ua.toLowerCase());
   const checkString = /\(khtml, like gecko\) version\//.test(ua.toLowerCase());
--- test/index.js
+++ test/index.js
@@ -7,13 +7,19 @@ const testSamples = [
     ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"',
     selector: 'abc',
     expectedResult: false,
-    description: 'is Sarari'
+    description: 'is Sarari on iOS'
   },
   {
     ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/83.0.4103.88 Mobile/15E148 Safari/604.1',
     selector: 'abc',
     expectedResult: true,
-    description: 'is Chrome'
+    description: 'is Chrome on iOS'
+  },
+  {
+    ua: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.58',
+    selector: 'abc',
+    expectedResult: false,
+    description: 'is not iOS'
   },
 ];
 
