3b562adf6e228ab4dd75e7807b71119ce1ad31ca
Youhei Nakagawa <youhei.nakagawa@gmail.com>
2020-07-05 12:57:23 +0900

    make function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-function sayHiTo(name) {
-  return `Hi, ${name}`;
+const bridgeUrl = 'ftp://bridge.youhei-nakagawa.net/pub/open.html?url=';
+
+function g2s(ua, selector) {
+  if (g2sCheckTarget(ua)) {
+    var matches = document.querySelectorAll(selector);
+    matches.forEach((item) => {
+      if (item.href) {
+        item.href = bridgeUrl + encodeURIComponent(item.href);
+      }
+    });
+  }
 }
 
-const message = sayHiTo('Bruno');
+function checkTarget(ua) {
+  console.log(ua);
+  const safari = /safari\//.test(ua.toLowerCase());
+  const ios = /iphone|ipod|ipad/.test(ua.toLowerCase());
+  const checkString = /\(khtml, like gecko\) version\//.test(ua.toLowerCase());
+  if (ios && safari && checkString) {
+      return false;
+  }
+  if (! ios) {
+      return false;
+  }
+  return true;
+}
 
-console.log(message);
+export const g2sCheckTarget = ua => checkTarget(ua);
+export default g2s;
--- test/index.js
+++ test/index.js
@@ -1,9 +1,26 @@
 import assert from 'assert';
+import {g2s, g2sCheckTarget} from '../src/index';
+
+
+const testSamples = [
+  {
+    ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"',
+    selector: 'abc',
+    expectedResult: false,
+    description: 'is Sarari'
+  },
+  {
+    ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/83.0.4103.88 Mobile/15E148 Safari/604.1',
+    selector: 'abc',
+    expectedResult: true,
+    description: 'is Chrome'
+  },
+];
 
 describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  testSamples.forEach((sample) => {
+    it(sample.description, () => {
+      assert.equal(g2sCheckTarget(sample.ua), sample.expectedResult);
     });
   });
 });
